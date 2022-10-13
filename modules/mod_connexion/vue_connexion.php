0ac019d677c11e1c5f89e505bab6978427e0c814
YanisDj <yanisdjahnit03@gmail.com>
2022-10-13 12:03:43 +0200

    sécurisation de la Connexion

--- modules/mod_connexion/vue_connexion.php
+++ modules/mod_connexion/vue_connexion.php
@@ -8,10 +8,14 @@ class VueConnexion extends VueGenerique{
     
 
     public function form_connexion(){
+        $token = uniqid(rand(), true);       
+        $_SESSION['token'] = $token;
+        $_SESSION['token_time'] = time();
         echo'<form action="index.php?module=mod_connexion&action=Connexion" method="POST">
 	        <p>login :</p> <input type="text" name="login" maxlength="50"/>
             <p>mot de passe :</p> <input type="password" name="mdp" maxlength="50"/>
             <input type="submit"/>
+            <input type="hidden" name="token" id="token" value="'.$token.'"/>
             </form>';
     }
 
