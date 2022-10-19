c06311f2dda4cf727ca670c4c3875be0461f5506
YanisDj <yanisdjahnit03@gmail.com>
2022-10-19 13:43:58 +0200

    changement inscip

--- modules/mod_inscription/modele_inscription.php
+++ modules/mod_inscription/modele_inscription.php
@@ -8,13 +8,15 @@ class ModeleInscription extends Connexion{
 
 
         public function ajoutInscription(){
-            $ajouteradhe = parent::$bdd -> prepare('INSERT INTO Adherent (sexe,nom,prenom,date_de_naissance,adresse_mail,numero_de_telephone,numero_de_licence,login,mot_de_passe,adresse) VALUES (?,?,?,?,?,?,?,?,?,?)');
-            $ajouteradhe->execute(array($_POST['sexe'],$_POST['nom'],$_POST['prenom'],$_POST['date_de_naissance'],$_POST['adresse_mail'],$_POST['numero_de_telephone'],$_POST['numero_de_licence'],$_POST['login'],password_hash($_POST['mot_de_passe'],PASSWORD_DEFAULT),$_POST['adresse']));    
             $ajouterville = parent::$bdd -> prepare('INSERT INTO Ville (Code_postal,ville) VALUES (?,?)');
             $ajouterville->execute(array($_POST["Code_postal"],$_POST["ville"]));
-            $ajouter = parent::$bdd-> prepare('INSERT INTO Info_inscription (reinscription,profession,nationalite,saison,club) VALUES (?,?,?,?,?)');
-            $ajouter->execute(array($_POST["reinscription"],$_POST["profession"],$_POST["nationalite"],$_POST["saison"],$_POST["club"]));
-            var_dump($_POST["reinscription"],$_POST["profession"],$_POST["nationalite"],$_POST["saison"],$_POST["club"]);
+            $id_ville = parent::$bdd->lastInsertId();
+            $ajouteradhe = parent::$bdd -> prepare('INSERT INTO adherent (sexe,nom,prenom,date_de_naissance,adresse_mail,numero_de_telephone,numero_de_licence,login,mot_de_passe,adresse,ID_ville) VALUES (?,?,?,?,?,?,?,?,?,?,?)');
+            $ajouteradhe->execute(array($_POST['sexe'],$_POST['nom'],$_POST['prenom'],$_POST['date_de_naissance'],$_POST['adresse_mail'],$_POST['numero_de_telephone'],$_POST['numero_de_licence'],$_POST['login'],password_hash($_POST['mot_de_passe'],PASSWORD_DEFAULT),$_POST['adresse'],$id_ville));    
+            $id_adherent = parent::$bdd->lastInsertId();
+            $ajouter = parent::$bdd-> prepare('INSERT INTO info_inscription (reinscription,profession,nationalite,saison,club,ID_Adherent) VALUES (?,?,?,?,?,?)');
+            $ajouter->execute(array($_POST["reinscription"],$_POST["profession"],$_POST["nationalite"],$_POST["saison"],$_POST["club"],$id_adherent));
+            var_dump($_POST['sexe'],$_POST["reinscription"]);
         }
     
     }
--- modules/mod_inscription/vue_inscription.php
+++ modules/mod_inscription/vue_inscription.php
@@ -8,23 +8,23 @@ class VueInscription extends VueGenerique{
 
     public function form_inscription(){
         echo'<form action="index.php?module=mod_inscription&action=ajout" method="POST">
-	        <p>Genre*:</p> <input type="text" name="sexe" maxlength="50"/>
-            <p>Nom* :</p> <input type="text" name="nom" maxlength="50"/>
-            <p>Prénom* :</p> <input type="text" name="prenom" maxlength="50"/>
-            <p>Date de naissance*:</p> <input type="date" name="date_de_naissance" maxlength="50"/>
-            <p>Adresse mail*:</p> <input type="email" name="adresse_mail" maxlength="50"/>
-            <p>Numéro de téléphone* :</p> <input type="tel" name="numero_de_telephone" maxlength="50"/>
-            <p>Adresse* :</p> <input type="text" name="adresse" maxlength="50"/>
-            <p>Code postal* :</p> <input type="text" name="Code_postal" maxlength="50"/>
-            <p>Ville* :</p> <input type="text" name="ville" maxlength="50"/>
-            <p>Login*:</p> <input type="text" name="login" maxlength="50"/>
-            <p>Votre mot de passe* :</p> <input type="password" name="mot_de_passe" maxlength="50"/>
-            <p>Numéro de licence (si ancien licencier) :</p> <input type="tel" name="numero_de_licence" maxlength="50"/>
-            <p>Reinscription:</p> <input type="text" name="reinscription" maxlength="50"/>
-            <p>Profession*:</p> <input type="text" name="profession" maxlength="50"/>
-            <p>Nationalité*:</p> <input type="text" name="nationalite" maxlength="50"/>
-            <p>Saison*:</p> <input type="text" name="saison" maxlength="50"/>
-            <p>Club*:</p> <input type="text" name="club" maxlength="50"/>
+	        <p>Genre*:</p> <input type="text" name="sexe" required="required" maxlength="50"/>
+            <p>Nom* :</p> <input type="text" name="nom" required="required" maxlength="50"/>
+            <p>Prénom* :</p> <input type="text" name="prenom" required="required" maxlength="50"/>
+            <p>Date de naissance*:</p> <input type="date" name="date_de_naissance" required="required" maxlength="50"/>
+            <p>Adresse mail*:</p> <input type="email" name="adresse_mail" required="required" maxlength="50"/>
+            <p>Numéro de téléphone* :</p> <input type="tel" name="numero_de_telephone" required="required" maxlength="50"/>
+            <p>Adresse* :</p> <input type="text" name="adresse" required="required" maxlength="50"/>
+            <p>Code postal* :</p> <input type="text" name="Code_postal" required="required" maxlength="50"/>
+            <p>Ville* :</p> <input type="text" name="ville" required="required" maxlength="50"/>
+            <p>Login*:</p> <input type="text" name="login" required="required" maxlength="50"/>
+            <p>Votre mot de passe* :</p> <input type="password" name="mot_de_passe" required="required" maxlength="50"/>
+            <p>Numéro de licence (si ancien licencier) :</p> <input type="tel" name="numero_de_licence" required="required" maxlength="50"/>
+            <p>Reinscription:</p> <input type="text" name="reinscription" required="required" maxlength="50"/>
+            <p>Profession*:</p> <input type="text" name="profession" required="required" maxlength="50"/>
+            <p>Nationalité*:</p> <input type="text" name="nationalite" required="required" maxlength="50"/>
+            <p>Saison*:</p> <input type="text" name="saison" required="required" maxlength="50"/>
+            <p>Club*:</p> <input type="text" name="club" required="required" maxlength="50"/>
             <input type="submit"/>
             </form>';
     }    
