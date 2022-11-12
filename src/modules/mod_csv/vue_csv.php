55679b6bae424b00ff5fcd6cd7f8ee4b5776928f
Bilel AIT ALLA <bilel.aitalla66@gmail.com>
2022-11-12 16:53:41 +0100

    Finalisation de la génération du CSV selon les besoins Clients (partie paiement à ajouter)

--- src/modules/mod_csv/vue_csv.php
+++ src/modules/mod_csv/vue_csv.php
@@ -11,8 +11,8 @@ class VueCSV extends VueGenerique {
 	public function afficherGenererCSV() {
 		echo '<h1> Génération du fichier CSV </h1>';
 		echo '<p> <a href="index.php?module=csv&action=generate"> Générer un nouveau fichier CSV </a> </p>';
-		if (file_exists("fichiers/csv_adherents/adherents.csv")) {
-			echo '<p><a href="./fichiers/csv_adherents/adherents.csv" target="_blank">Télécharger le fichier CSV </a></p>';
+		if (file_exists("fichiers/csv_adherents/adherents.xlsx")) {
+			echo '<p><a href="./fichiers/csv_adherents/adherents.xlsx" target="_blank">Télécharger le fichier CSV </a></p>';
 		}
 	}
 }
