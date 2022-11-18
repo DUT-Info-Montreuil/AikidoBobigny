19a843a84f5a5d1b6b4cc8e48ce1338cbcaf4209
Bilel AIT ALLA <bilel.aitalla66@gmail.com>
2022-11-18 13:40:44 +0100

    erreur admin

--- src/modules/mod_admin/script.js
+++ src/modules/mod_admin/script.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
     $('.button').click(function(){
         var clickBtnValue = $(this).val();
-        var ajaxurl = 'ajax.php',
-        data =  {'action': clickBtnValue};
+        var ajaxurl = 'index.php?module=admin',
+        data =  {'function': clickBtnValue};
         $.post(ajaxurl, data, function (response) {
             alert("action performed successfully");
         });
