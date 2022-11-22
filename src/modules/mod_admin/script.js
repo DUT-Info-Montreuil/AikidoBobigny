c714f323fb2cf88057ccc7960194b4faa24234bb
YanisDj <yanisdjahnit03@gmail.com>
2022-11-22 13:08:09 +0100

    probleme dans le js

--- src/modules/mod_admin/script.js
+++ src/modules/mod_admin/script.js
@@ -1,13 +1,21 @@
-$(document).ready(function(){
+$(document).ready(function(){    
     $('.button').click(function(){
         var clickBtnValue = $(this).val();
-        var token = $("#token").val();
-        var id = $("ID_adherent").val();
-        alert(id);
-        var ajaxurl = '../src/modules/mod_admin/admin.php',
-        data =  {'function': clickBtnValue, 'token': token, 'ID_adherent': id};
-        $.post(ajaxurl, data, function (response) {
-            alert("action performed successfully");
-        });
+        var token = $("#token").val();    
+        //Le probleme c'est l'id il recupere 1
+        var id;
+        if ($(this).val() == clickBtnValue) {
+            id = $("#ID_adherent").val();
+        }
+        var ajaxurl = '../src/modules/mod_admin/admin.php';
+
+        $.ajax({
+            type: "POST",
+            url: ajaxurl,
+            data: {'function': clickBtnValue, 'token': token, 'ID_adherent' : id},
+            success: function(data) {
+                alert("action performed successfully");
+            }
+        })
     });
 });
