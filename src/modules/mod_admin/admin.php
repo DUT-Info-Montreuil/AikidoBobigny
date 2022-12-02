6b5f937122d8f9c33c761f5b923ff0df95d214f6
YanisDj <yanisdjahnit03@gmail.com>
2022-12-02 10:56:54 +0100

    securisation faq

--- src/modules/mod_admin/admin.php
+++ src/modules/mod_admin/admin.php
@@ -4,7 +4,7 @@ session_start();
 require_once("../../connexion.php");
 Connexion::initConnexion();
             if($_POST["actionadherent"]==1){
-
+                        
                         if (isset($_POST["targetID"])) {
                             $req1 = Connexion::getConnexion()->prepare('DELETE from info_inscription where ID_adherent= ? ');
                             $req1->execute(array($_POST['targetID']));
